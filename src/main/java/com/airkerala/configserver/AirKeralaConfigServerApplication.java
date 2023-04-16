2ee570fe9325f3f56bc2a04ecb1063b556b91e18
SubinBMathew95 <130916190+SubinBMathew95@users.noreply.github.com>
2023-04-16 21:17:00 +0530

    Initial Commit

---
+++ src/main/java/com/airkerala/configserver/AirKeralaConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.airkerala.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirKeralaConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirKeralaConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/airkerala/configserver/AirKeralaConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airkerala.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirKeralaConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
